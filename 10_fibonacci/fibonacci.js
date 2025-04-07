41290255b8153809d8ee7e8e4c2ae2bbf10174d9
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-04-07 19:39:14 +0200

    Assignment complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let term1 = 1;
+    let term2 = 1;
+    let newTerm = 0;
 
+    if (typeof num == "string") num = Number.parseInt(num);
+    if (num < 0) return "OOPS";
+    if (num === 0) return 0;
+    // first two terms are known to be 1
+    if (num < 3) return 1;
+    if (num >= 3) newTerm = term1 + term2;
+    if (num > 3) {
+        for (let i = 4; i <= num; i++) {
+            term1 = term2;
+            term2 = newTerm;
+            newTerm = term1 + term2;
+        }
+    }
+    return newTerm;
 };
 
 // Do not edit below this line
