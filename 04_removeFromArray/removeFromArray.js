b253132c26aadb1d9e429336d817f311a0998bfb
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-04-07 14:24:00 +0200

    Completed assignment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...restArgs) {
+    const newArr = arr;
+    
+    //Outer loop to iterate over all items to be removed (restArgs)
+    for (let i = 0; i < restArgs.length; i++) {
+        //only run inner loop if the array containes at least one instance of the item to be removed
+        if (newArr.includes(restArgs[i])) {
+            //inner loop to iterate over the array to remove all items needed (newArr)
+            for (let j = 0; j < newArr.length; j++) {
+                if (newArr[j] === restArgs[i]) {
+                    newArr.splice(j,1);
+                    j--; // reduce j by 1 because array length reduced by 1, otherwise would miss checking the next element
+                }
+            }
+        }
+    }
+    return newArr; 
 };
 
 // Do not edit below this line
