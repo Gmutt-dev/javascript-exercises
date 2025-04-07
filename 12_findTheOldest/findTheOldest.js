578396d44f2571d4f02093e6334387fc96c77319
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-04-07 20:58:50 +0200

    Assignment completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let personAge = 0;
+    let oldestPerson = Object;
+    let oldestAge = 0;
+    const date = new Date();
+    people.forEach(person => {
+        personAge = (person.yearOfDeath || date.getFullYear()) - person.yearOfBirth;
+        if (personAge > oldestAge) {
+            oldestAge = personAge;
+            oldestPerson = person;
+        }
+    })
+    return oldestPerson;
 };
 
 // Do not edit below this line
