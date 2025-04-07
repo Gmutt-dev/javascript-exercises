41a00647aeef9d4c02b429f7fd4f148ebf27a941
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-04-07 11:43:20 +0200

    Completed assignment

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR';
+    let catString = "";
+    for(let i = 0; i < num; i++) {
+        catString += string;
+    }
+    return catString;
 };
 
 // Do not edit below this line
