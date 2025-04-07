32df475290f8b10cfad8b9abd1cafac9abaa67de
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-04-07 16:30:40 +0200

    Completed assignment

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+	arr.forEach((val) => {sum += val});
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  arr.forEach((val) => {product *= val});
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(num) {
+  // let product = 1;
+  // for (let i = num; i > 1; i--) {
+  //   product *= i;
+  // }
+  // return product;
+  
+  //recursive answer
+  if (num === 1 || num === 0) return 1;
+  else return num * factorial(num-1);
+  }
 
 // Do not edit below this line
 module.exports = {
