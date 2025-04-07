36abaa151aebbee41e8db4be1b5c407a8506f150
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-04-07 15:26:32 +0200

    Assignment completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //use truthy and falsy results (mods) to determite if a leap year or not
+    if (!(year % 4) && (!(year % 400) || (year % 100))) return true;
+    else return false;
 };
 
 // Do not edit below this line
